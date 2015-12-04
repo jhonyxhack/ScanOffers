8b535b5d04c46c725e2c4db5dc153c7b6bf09952
jhonyxhack <juan_alberto50@hotmail.com>
2015-12-04 17:41:57 -0600

    aacc df

---
+++ app/src/main/java/Entidades/E_Cliente.java
@@ -0,0 +1,17 @@
+    package Entidades;
+
+    /**
+     * Created by Romina on 04/12/2015.
+     */
+    public class E_Cliente {
+
+
+            public  int User_id;
+            public  String User_nombre;
+          public    String User_apellido;
+            public  String User_email;
+               public  String User_password;
+            public  String User_edad;
+
+
+    }
