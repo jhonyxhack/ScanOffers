a1a4e698e479ee2d164fa82410dc30587e09e42d
Juan Alberto Escobar Hernandez <juan_alberto50@hotmail.com>
2015-12-07 20:42:14 -0600

    Actualización de la Clase E_clinte;

--- app/src/main/java/Entidades/E_Cliente.java
+++ app/src/main/java/Entidades/E_Cliente.java
@@ -5,13 +5,22 @@
      */
     public class E_Cliente {
 
+        public  int User_id=0;
+        public  String User_nombre="";
+        public  String User_apellido="";
+        public  String User_email="";
+        public  String User_password="";
+        public  String User_edad="";
 
-            public  int User_id;
-            public  String User_nombre;
-          public    String User_apellido;
-            public  String User_email;
-               public  String User_password;
-            public  String User_edad;
+        public E_Cliente()
+        {
+            User_id = 0 ;
+        }
+
+
+
+
+
 
 
     }
